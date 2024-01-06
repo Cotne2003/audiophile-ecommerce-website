1a07783a3a3612262476e300936139ef89c71954
Cotne Basiashvili <basiashvilic@gmail.com>
2024-01-06 16:00:06 +0400

    added final check function

---
+++ components/checkout/finalCheck/FinalCheck.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import styles from "./FinalCheckStyles.module.css";
+import { SecondTitle } from "@/components/texts/Titles";
+import test from "@/public/assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg";
+import { CheckoutBtn } from "@/components/header/cartSpace/CartSpaceStyles";
+import Link from "next/link";
+import { useRecoilValue } from "recoil";
+import { Product, cartData } from "@/app/states";
+
+const FinalCheck = () => {
+  const cart = useRecoilValue<Product[]>(cartData);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const cartModel = [...cart];
+    const sumOfProducts = cartModel.reduce(
+      (sum, product) => Number(product.price) * product.count + sum,
+      0
+    );
+    setTotal(sumOfProducts);
+  }, [cart]);
+  return (
+    <div className={styles.finalContainer}>
+      <div className={styles.finalCheckWrapper}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="64"
+          height="64"
+          viewBox="0 0 64 64"
+          fill="none"
+        >
+          <circle cx="32" cy="32" r="32" fill="#D87D4A" />
+          <path
+            d="M20.7539 33.3328L27.5054 40.0843L43.3085 24.2812"
+            stroke="white"
+            strokeWidth="4"
+          />
+        </svg>
+        <SecondTitle>THANK YOU FOR YOUR ORDER</SecondTitle>
+        <p className={styles.firstPar}>
+          You will receive an email confirmation shortly.
+        </p>
+        <div className={styles.itemsCont}>
+          <div className={styles.firstItem}>
+            <div className={styles.productInfo}>
+              <div className={styles.imgPriceName}>
+                <img src={cart[0].imgUrl} alt="" />
+                <div>
+                  <h3>{cart[0].title}</h3>
+                  <p>${cart[0].price}</p>
+                </div>
+              </div>
+              <p>x1</p>
+            </div>
+            <p className={styles.otherItems}>and 2 other items</p>
+          </div>
+          <div className={styles.secondItem}>
+            <p>grand total</p>
+            <p>${total + 50}</p>
+          </div>
+        </div>
+        <Link href={"/"}>
+          <CheckoutBtn>BACK TO HOME</CheckoutBtn>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FinalCheck;
--- components/checkout/inputsAndPayment/InputsAndPayment.tsx
+++ components/checkout/inputsAndPayment/InputsAndPayment.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CheckoutContainer,
   InputTitle,
@@ -11,6 +11,7 @@ import {
 import { SecondTitle } from "@/components/texts/Titles";
 import { useForm } from "react-hook-form";
 import { CheckoutBtn } from "@/components/header/cartSpace/CartSpaceStyles";
+import FinalCheck from "../finalCheck/FinalCheck";
 
 type DataType = {
   name: string;
@@ -25,199 +26,229 @@ type DataType = {
 };
 
 const InputsAndPayment = () => {
+  const [finalCheck, setFinalCheck] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<DataType>();
-  const onSubmit = () => {};
+  const onSubmit = () => {
+    if (
+      errors.name?.message === undefined &&
+      errors.email?.message === undefined &&
+      errors.phone?.message === undefined &&
+      errors.address?.message === undefined &&
+      errors.zip?.message === undefined &&
+      errors.city?.message === undefined &&
+      errors.country?.message === undefined &&
+      errors.eMoneyNum?.message === undefined &&
+      errors.eMoneyPin?.message === undefined
+    ) {
+      setFinalCheck(true);
+    }
+  };
   return (
-    <CheckoutContainer>
-      <SecondTitle>CHECKOUT</SecondTitle>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <OrangeTxtWrapper>
-            <OrangeTxt>billing details</OrangeTxt>
-          </OrangeTxtWrapper>
-          <InputsContainer>
-            <div>
-              <InputTitle>Name</InputTitle>
-              <StyledInput
-                type="text"
-                placeholder="Cotne Basiashvili"
-                {...register("name", {
-                  required: {
-                    value: true,
-                    message: "name cannot be empty",
-                  },
-                })}
-                style={
-                  errors.name?.message ? { outline: "2px solid #FF7979" } : {}
-                }
-              />
-            </div>
-            <div>
-              <InputTitle>Email Address</InputTitle>
-              <StyledInput
-                type="email"
-                placeholder="basiashvilic@gmail.com"
-                {...register("email", {
-                  required: {
-                    value: true,
-                    message: "email cannot be empty",
-                  },
-                  pattern: {
-                    value:
-                      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                    message: "Looks like this is not an email",
-                  },
-                })}
-                style={
-                  errors.email?.message ? { outline: "2px solid #FF7979" } : {}
-                }
-              />
-            </div>
-            <div>
-              <InputTitle>Phone Number</InputTitle>
-              <StyledInput
-                type="number"
-                placeholder="+1 202-555-0136"
-                {...register("phone", {
-                  required: {
-                    value: true,
-                    message: "phone cannot be empty",
-                  },
-                })}
-                style={
-                  errors.phone?.message ? { outline: "2px solid #FF7979" } : {}
-                }
-              />
-            </div>
-          </InputsContainer>
-        </div>
-
-        <div>
-          <OrangeTxtWrapper>
-            <OrangeTxt>shipping info</OrangeTxt>
-          </OrangeTxtWrapper>
+    <>
+      <CheckoutContainer>
+        <SecondTitle>CHECKOUT</SecondTitle>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <InputTitle>Address</InputTitle>
-            <StyledInput
-              type="text"
-              placeholder="1137 Williams Avenue"
-              {...register("address", {
-                required: {
-                  value: true,
-                  message: "address cannot be empty",
-                },
-              })}
-              style={
-                errors.address?.message ? { outline: "2px solid #FF7979" } : {}
-              }
-            />
-          </div>
-          <InputsContainerWrapper>
+            <OrangeTxtWrapper>
+              <OrangeTxt>billing details</OrangeTxt>
+            </OrangeTxtWrapper>
             <InputsContainer>
               <div>
-                <InputTitle>ZIP Code</InputTitle>
+                <InputTitle>Name</InputTitle>
+                <StyledInput
+                  type="text"
+                  placeholder="Cotne Basiashvili"
+                  {...register("name", {
+                    required: {
+                      value: true,
+                      message: "name cannot be empty",
+                    },
+                  })}
+                  style={
+                    errors.name?.message ? { outline: "2px solid #FF7979" } : {}
+                  }
+                />
+              </div>
+              <div>
+                <InputTitle>Email Address</InputTitle>
+                <StyledInput
+                  type="email"
+                  placeholder="basiashvilic@gmail.com"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "email cannot be empty",
+                    },
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                      message: "Looks like this is not an email",
+                    },
+                  })}
+                  style={
+                    errors.email?.message
+                      ? { outline: "2px solid #FF7979" }
+                      : {}
+                  }
+                />
+              </div>
+              <div>
+                <InputTitle>Phone Number</InputTitle>
                 <StyledInput
                   type="number"
-                  placeholder="10001"
-                  {...register("zip", {
+                  placeholder="+1 202-555-0136"
+                  {...register("phone", {
                     required: {
                       value: true,
-                      message: "zip cannot be empty",
+                      message: "phone cannot be empty",
                     },
                   })}
                   style={
-                    errors.zip?.message ? { outline: "2px solid #FF7979" } : {}
-                  }
-                />
-              </div>
-              <div>
-                <InputTitle>City</InputTitle>
-                <StyledInput
-                  type="text"
-                  placeholder="Tbilisi"
-                  {...register("city", {
-                    required: {
-                      value: true,
-                      message: "city cannot be empty",
-                    },
-                  })}
-                  style={
-                    errors.city?.message ? { outline: "2px solid #FF7979" } : {}
-                  }
-                />
-              </div>
-              <div>
-                <InputTitle>Country</InputTitle>
-                <StyledInput
-                  type="text"
-                  placeholder="Georgia"
-                  {...register("country", {
-                    required: {
-                      value: true,
-                      message: "country cannot be empty",
-                    },
-                  })}
-                  style={
-                    errors.country?.message
+                    errors.phone?.message
                       ? { outline: "2px solid #FF7979" }
                       : {}
                   }
                 />
               </div>
             </InputsContainer>
-          </InputsContainerWrapper>
-        </div>
+          </div>
 
-        <div>
-          <OrangeTxtWrapper>
-            <OrangeTxt>payment details</OrangeTxt>
-          </OrangeTxtWrapper>
-          <InputsContainer>
+          <div>
+            <OrangeTxtWrapper>
+              <OrangeTxt>shipping info</OrangeTxt>
+            </OrangeTxtWrapper>
             <div>
-              <InputTitle>e-Money Number</InputTitle>
+              <InputTitle>Address</InputTitle>
               <StyledInput
-                type="number"
-                placeholder="238521993"
-                {...register("eMoneyNum", {
+                type="text"
+                placeholder="1137 Williams Avenue"
+                {...register("address", {
                   required: {
                     value: true,
-                    message: "eMoneyNum cannot be empty",
+                    message: "address cannot be empty",
                   },
                 })}
                 style={
-                  errors.eMoneyNum?.message
+                  errors.address?.message
                     ? { outline: "2px solid #FF7979" }
                     : {}
                 }
               />
             </div>
-            <div>
-              <InputTitle>e-Money PIN</InputTitle>
-              <StyledInput
-                type="number"
-                placeholder="6891"
-                {...register("eMoneyPin", {
-                  required: {
-                    value: true,
-                    message: "eMoneyPin cannot be empty",
-                  },
-                })}
-                style={
-                  errors.eMoneyPin?.message
-                    ? { outline: "2px solid #FF7979" }
-                    : {}
-                }
-              />
-            </div>
-          </InputsContainer>
-        </div>
-        <CheckoutBtn style={{ marginTop: "50px" }}>CONTINUE & PAY</CheckoutBtn>
-      </form>
-    </CheckoutContainer>
+            <InputsContainerWrapper>
+              <InputsContainer>
+                <div>
+                  <InputTitle>ZIP Code</InputTitle>
+                  <StyledInput
+                    type="number"
+                    placeholder="10001"
+                    {...register("zip", {
+                      required: {
+                        value: true,
+                        message: "zip cannot be empty",
+                      },
+                    })}
+                    style={
+                      errors.zip?.message
+                        ? { outline: "2px solid #FF7979" }
+                        : {}
+                    }
+                  />
+                </div>
+                <div>
+                  <InputTitle>City</InputTitle>
+                  <StyledInput
+                    type="text"
+                    placeholder="Tbilisi"
+                    {...register("city", {
+                      required: {
+                        value: true,
+                        message: "city cannot be empty",
+                      },
+                    })}
+                    style={
+                      errors.city?.message
+                        ? { outline: "2px solid #FF7979" }
+                        : {}
+                    }
+                  />
+                </div>
+                <div>
+                  <InputTitle>Country</InputTitle>
+                  <StyledInput
+                    type="text"
+                    placeholder="Georgia"
+                    {...register("country", {
+                      required: {
+                        value: true,
+                        message: "country cannot be empty",
+                      },
+                    })}
+                    style={
+                      errors.country?.message
+                        ? { outline: "2px solid #FF7979" }
+                        : {}
+                    }
+                  />
+                </div>
+              </InputsContainer>
+            </InputsContainerWrapper>
+          </div>
+
+          <div>
+            <OrangeTxtWrapper>
+              <OrangeTxt>payment details</OrangeTxt>
+            </OrangeTxtWrapper>
+            <InputsContainer>
+              <div>
+                <InputTitle>e-Money Number</InputTitle>
+                <StyledInput
+                  type="number"
+                  placeholder="238521993"
+                  {...register("eMoneyNum", {
+                    required: {
+                      value: true,
+                      message: "eMoneyNum cannot be empty",
+                    },
+                  })}
+                  style={
+                    errors.eMoneyNum?.message
+                      ? { outline: "2px solid #FF7979" }
+                      : {}
+                  }
+                />
+              </div>
+              <div>
+                <InputTitle>e-Money PIN</InputTitle>
+                <StyledInput
+                  type="number"
+                  placeholder="6891"
+                  {...register("eMoneyPin", {
+                    required: {
+                      value: true,
+                      message: "eMoneyPin cannot be empty",
+                    },
+                  })}
+                  style={
+                    errors.eMoneyPin?.message
+                      ? { outline: "2px solid #FF7979" }
+                      : {}
+                  }
+                />
+              </div>
+            </InputsContainer>
+          </div>
+          <CheckoutBtn style={{ marginTop: "50px" }}>
+            CONTINUE & PAY
+          </CheckoutBtn>
+        </form>
+      </CheckoutContainer>
+      <FinalCheck />
+    </>
   );
 };
 
