b4496e554e0133353f90af03585d0f811a12d44f
Cotne Basiashvili <basiashvilic@gmail.com>
2024-01-08 04:03:19 +0400

    fixed little bug

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
           Audiophile is an all in one stop to fulfill your audio needs. We're a
           small team of music lovers and sound specialists who are devoted to
           helping you get the most out of personal audio. Come and visit our
-          demo facility - we’re open 7 days a week.
+          demo facility - were open 7 days a week.
         </p>
         <div className={styles.icons}>
           <img src={FBicon.src} alt="FBicon" className={styles.icon} />
